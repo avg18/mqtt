a40d60ced57446c800866821aaf500134d0aa4ca
Oliver Nagel <rulestormer@yahoo.de>
2018-04-08 23:40:14 +0200

    first mqtt stuff

--- temperatursensor/src/main/java/de/avg/naol/temperatursensor/Main.java
+++ temperatursensor/src/main/java/de/avg/naol/temperatursensor/Main.java
@@ -1,13 +1,18 @@
 package de.avg.naol.temperatursensor;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, MqttException {
         TempSensor tempSensor = new TempSensor();
+        MqttClient client = new MqttClient("tcp://localhost:1000", "temperatursensor");
 
-        while(true) {
-            Thread.sleep(2000);
-            System.out.println(tempSensor.getTemperature());
+        while (true) {
+            Thread.sleep(3000);
+            client.publish("/livingroom/temperature", new MqttMessage(tempSensor.getTemperature().getBytes()));
         }
     }
 }
